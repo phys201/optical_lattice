7eaab65a8dfab8cfb6e4a46a607d6064e7ac45fd
eknall <51799857+eknall@users.noreply.github.com>
2020-04-13 19:03:24 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='optical_lattice',
+      version='0.1',
+      description='Resolve atom occupations in an optical lattice',
+      url='https://github.com/phys201/optical_lattice',
+      author='Furkan Ozturk, Can Knaut, Erik Knall',
+      author_email='erikknall@g.harvard.edu',
+      license='GNU GPL',
+      packages=['optical_lattice'],
+      zip_safe=False)
