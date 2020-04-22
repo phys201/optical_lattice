f3bac60d0a985f97f075ace43934001ce585f461
sfozturk61 <sfozturk61@gmail.com>
2020-04-21 20:21:39 -0400

    Update raw_data_analyzer.py

--- optical_lattice/raw_data_analyzer.py
+++ optical_lattice/raw_data_analyzer.py
@@ -31,7 +31,7 @@ class LatticeImageAnalyzer():
         self.shift_left = shift_left
         self.threshold_buffer = threshold_buffer
     
-    def import_lattice(self, raw_image_path, shot_number):
+    def _import_lattice(self, raw_image_path, shot_number):
         '''Load lattice image from an hdf file, convert into a numpy array and select a shot for analysis'''
 
         file = h5py.File(raw_image_path, 'r') 
@@ -41,7 +41,7 @@ class LatticeImageAnalyzer():
         self.raw_img_array = raw_img_array
         
     
-    def import_PSF(self, psf_path):
+    def _import_PSF(self, psf_path):
         '''Load psf image from a pkl file, convert it into a numpy array'''
 
         with open(psf_path, 'rb') as f:
@@ -141,11 +141,11 @@ class LatticeImageAnalyzer():
         
         plt.show()
     
-    def analyze_raw_data(self, raw_image, psf, plot, plot_hist):
+    def analyze_raw_data(self, plot, plot_hist):
     
-        rotated = self._rotate_image(raw_image, self.angle)
+        rotated = self._rotate_image(self._import_lattice(), self.angle)
         rotated_roi = rotated[self.roi[0]:self.roi[1], self.roi[2]:self.roi[3]]
-        deconvolved = self._wiener_deconvolve(rotated_roi, psf)
+        deconvolved = self._wiener_deconvolve(rotated_roi, self._import_PSF)
         shifted = self._shift_image(deconvolved, self.shift_up, self.shift_left)
         site_counts, threshold = self._find_threshold(shifted, self.M, plot=plot_hist)
         binarized = self._binarize_image(site_counts, threshold, self.threshold_buffer)
