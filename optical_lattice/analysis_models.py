2b4855eea63755b268fe91c77c4f223d4b334387
Can Knaut <can.knaut@gmail.com>
2020-04-28 23:39:35 -0400

    Code cleanup

--- optical_lattice/analysis_models.py
+++ optical_lattice/analysis_models.py
@@ -1,10 +1,11 @@
+"""Define the first generation mixture model."""
 import pymc3 as pm
+
 import theano.tensor as tt
 
 
 def mixture_model_v0(x, y, std, xsite, ysite):
-    """Define the moxture model.
-    pymc3 odel
+    """Define the mixture model.
 
     Parameters
     ----------
@@ -15,39 +16,45 @@ def mixture_model_v0(x, y, std, xsite, ysite):
     std : float
         Gaussian width of the point spread function
     xsite : ndarray of ints
-        Two element array containing the lower and upper x edges of the given lattice site
+        Two element array containing the lower
+        and upper x edges of the given lattice site
     ysite : ndarray of ints
-        Two element array containing the lower and upper y edges of the given lattice site
-
-    Returns
-    -------
-
+        Two element array containing the
+        lower and upper y edges of the given lattice site
     """
     with pm.Model() as mixture_model_v0:
 
-        #Prior
-        P = pm.Uniform('P', lower=0, upper=1)
+        # Prior
+        p = pm.Uniform('P', lower=0, upper=1)
 
-        xc = (xsite[0]+xsite[1])/2 #x center of the site
-        yc = (ysite[0]+ysite[1])/2 #y center of the site
+        xc = (xsite[0]+xsite[1]) / 2  # x center of the site
+        yc = (ysite[0]+ysite[1]) / 2  # y center of the site
 
-        #Photons scattered from the atoms are Gaussian distributed
+        # Photons scattered from the atoms are Gaussian distributed
         atom_x = pm.Normal.dist(mu=xc, sigma=std).logp(x)
         atom_y = pm.Normal.dist(mu=yc, sigma=std).logp(y)
         atom = atom_x + atom_y
 
-        #Photons from the camera background are uniform distributed
-        background_x = pm.Uniform.dist(lower = xsite[0], upper = xsite[1]).logp(x)
-        background_y = pm.Uniform.dist(lower = ysite[0], upper = ysite[1]).logp(y)
+        # Photons from the camera background are uniform distributed
+        background_x = pm.Uniform.dist(
+            lower=xsite[0],
+            upper=xsite[1]
+        ).logp(x)
+
+        background_y = pm.Uniform.dist(
+            lower=ysite[0],
+            upper=ysite[1]
+        ).logp(y)
+
         background = background_x + background_y
 
-        #Log-likelihood
-        log_like = tt.log((P * tt.exp(atom) + (1-P) * tt.exp(background)))
+        # Log-likelihood
+        log_like = tt.log((p * tt.exp(atom) + (1-p) * tt.exp(background)))
 
         pm.Potential('logp', log_like.sum())
 
-    #MAP value
+    # MAP value
     map_estimate = pm.find_MAP(model=mixture_model_v0)
-    P_value = map_estimate["P"]
+    p_value = map_estimate["P"]
 
-    return P_value
+    return p_value
--- optical_lattice/lattice_generation.py
+++ optical_lattice/lattice_generation.py
@@ -1,6 +1,8 @@
+""" Lattice generation class definition."""
 import matplotlib as mpl
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 class GeneratedLatticeImage():
     ''' A generated data object
@@ -10,9 +12,11 @@ class GeneratedLatticeImage():
         M : integer
             number of camera pixels per lattice site along one direction (MxM)
         x_loc : ndarray of ints
-            x positions of all photon counts due to CCD dark counts and atom fluorescence
+            x positions of all photon counts due to CCD
+            dark counts and atom fluorescence
         y_loc : ndarray of ints
-            y positions of all photon counts due to CCD dark counts and atom fluorescence
+            y positions of all photon counts due to CCD
+            dark counts and atom fluorescence
         actual_lattice : 2d ndarray of ints
             2d grid representing true filling of optical lattice
         pixel_grid : 2d ndarray of ints
@@ -20,11 +24,23 @@ class GeneratedLatticeImage():
 
     '''
 
-    def __init__(self, N, M, N_atom, N_photon, CCD_resolution = 1024, lattice_origin = (400,600), std=10, N_backg=2000, lam_backg=200):
+    def __init__(
+        self,
+        N,
+        M,
+        N_atom,
+        N_photon,
+        CCD_resolution=1024,
+        lattice_origin=(400, 600),
+        std=10,
+        N_backg=2000,
+        lam_backg=200
+    ):
         '''
-        Generates positions of photon counts and CCD intensity values from the randomly placed atoms on a lattice and from Poissonian 
-        dark counts. Includes full CCD data with dedicated optical lattice region.
-        
+        Generates positions of photon counts and CCD intensity values from
+        the randomly placed atoms on a lattice and from Poissonian dark counts.
+         Includes full CCD data with dedicated optical lattice region.
+
         Parameters
         ----------
         N : integer
@@ -38,11 +54,13 @@ class GeneratedLatticeImage():
         CCD_resolution : integer
             number of pixels along one axis of CCD
         lattice_origi n: tuple of integers
-            top left corner of optical lattice region. Optical lattice region is (M*N)x(M*N) pixels large.
+            top left corner of optical lattice region.
+            Optical lattice region is (M*N)x(M*N) pixels large.
         std : float
             standard deviation of the Gaussian that is sampled from
         N_backg : integer
-            number of samples drawn from the Poisson distribution for the background noise
+            number of samples drawn from the Poisson distribution
+            for the background noise
         lam_back : float
             expectation interval of the Poisson dark count event
 
@@ -62,84 +80,111 @@ class GeneratedLatticeImage():
         self.std = std
         self.lattice_origin = lattice_origin
 
-        #Randomly place atoms on the lattice
-        atom_location = np.random.choice(np.arange(N*N), N_atom, replace=False) #pick atom position randomly from NxN array
+        # Randomly place atoms on the lattice
+        # pick atom position randomly from NxN array
+        atom_location = np.random.choice(np.arange(N*N), N_atom, replace=False)
 
-        actual_lattice = np.zeros((N,N))
-        atom_location_index = np.unravel_index(atom_location,(N,N))
+        actual_lattice = np.zeros((N, N))
+        atom_location_index = np.unravel_index(atom_location, (N, N))
 
-        #Store actual occupation of the atoms for future comparison with the inferred one
-        for x,y in zip(atom_location_index[0],atom_location_index[1]):
-            actual_lattice[y,x] = 1
+        # Store actual occupation of the atoms
+        # for future comparison with the inferred one
+        for x, y in zip(atom_location_index[0], atom_location_index[1]):
+            actual_lattice[y, x] = 1
             # Uncomment for inverted y
             # actual_lattice[N-y-1,x] = 1
 
-        atom_location_index = atom_location_index + np.zeros((2, N_atom))*M*N #convert the atom location number to x,y atom location index
-
+        # convert the atom location number to x,y atom location index
+        atom_location_index = atom_location_index + np.zeros((2, N_atom))*M*N
         atom_location_index = atom_location_index*M + M/2
-        x_index = atom_location_index[0,:] #atoms x location
-        y_index = N*M - atom_location_index[1,:] #atoms y location
+        x_index = atom_location_index[0, :]  # atoms x location
+        y_index = N*M - atom_location_index[1, :]  # atoms y location
 
-        pixel_grid = np.zeros((CCD_resolution,CCD_resolution))
+        pixel_grid = np.zeros((CCD_resolution, CCD_resolution))
 
-        #For each atom sample photons from a Gaussian centered on the lattice site, combine the x,y positions of the counts
+        # For each atom sample photons from a Gaussian centered on
+        # the lattice site, combine the x,y positions of the counts
         x_loc = np.array([])
         y_loc = np.array([])
         for i in range(N_atom):
-            xx, yy = np.random.multivariate_normal([x_index[i], y_index[i]], [[std, 0], [0, std]], N_photon).T #at each atom location sample N_photons from a Gaussian
-            # Round and cast photon positions to respect pixel postions 
+            # at each atom location sample N_photons from a Gaussian
+            xx, yy = np.random.multivariate_normal(
+                [x_index[i], y_index[i]],
+                [
+                    [std, 0],
+                    [0, std]
+                ],
+                N_photon
+            ).T
+            # Round and cast photon positions to respect pixel postions
             xx = np.rint(xx).astype(int) + lattice_origin[0]
             yy = np.rint(yy).astype(int) + lattice_origin[1]
-            x_loc = np.concatenate((x_loc, xx)) #combine the sampled x-locations for each atom
-            y_loc = np.concatenate((y_loc, yy)) #combine the sampled y-locations for each atom
+            x_loc = np.concatenate((x_loc, xx))  # combine the sampled x-locations for each atom
+            y_loc = np.concatenate((y_loc, yy))  # combine the sampled y-locations for each atom
 
-        #Generate dark counts which is the background noise of the camera. Combine dark photon locations with scattered photon locations.
-        CCD_x = np.arange(0, CCD_resolution, 1) #x-pixel locations
-        CCD_y = np.arange(0, CCD_resolution, 1) #y-pixel locations
-        dark_count = np.random.poisson(lam_backg, N_backg) #create dark counts sampling from a Poisson distribution, this gives numbers corresponding to number of dark counts
-        dark_count_location_x = np.random.choice(CCD_x, np.sum(dark_count), replace=True) #pick a random x location for the dark counts
-        dark_count_location_y = np.random.choice(CCD_y, np.sum(dark_count), replace=True) #pick a random y location for the dark counts
+        # Generate dark counts which is the background noise of the camera.
+        # Combine dark photon locations with scattered photon locations.
+        CCD_x = np.arange(0, CCD_resolution, 1)  # x-pixel locations
+        CCD_y = np.arange(0, CCD_resolution, 1)  # y-pixel locations
 
-        x_loc = np.concatenate((x_loc, dark_count_location_x)).astype(int) #combine the sampled x-locations from atoms and dark counts
-        y_loc = np.concatenate((y_loc, dark_count_location_y)).astype(int) #combine the sampled y-locations from atoms and dark counts
+        # create dark counts sampling from a Poisson distribution, this gives numbers corresponding to number of dark counts
+        dark_count = np.random.poisson(lam_backg, N_backg)
 
-        #convert counts to intensity values for each pixel
+        # pick a random x location for the dark counts
+        dark_count_location_x = np.random.choice(
+            CCD_x,
+            np.sum(dark_count),
+            replace=True
+        )
 
-        for x,y in zip(x_loc,y_loc):
-            if(x<CCD_resolution and y<CCD_resolution and x>0 and y>0):
+        # pick a random y location for the dark counts
+        dark_count_location_y = np.random.choice(
+            CCD_y, np.sum(dark_count),
+            replace=True
+        )
+
+        # Combine the sampled x-locations from atoms and dark counts
+        x_loc = np.concatenate((x_loc, dark_count_location_x)).astype(int)
+
+        # Combine the sampled y-locations from atoms and dark counts
+        y_loc = np.concatenate((y_loc, dark_count_location_y)).astype(int)
+
+        # Convert counts to intensity values for each pixel
+        for x, y in zip(x_loc, y_loc):
+            if(x < CCD_resolution and y < CCD_resolution and x > 0 and y > 0):
                 # pixel_grid[CCD_resolution-y-1,x] += 1
-                pixel_grid[y,x] += 1
+                pixel_grid[y, x] += 1
 
-        #Shift and scale counts to mimic data from CCD
+        # Shift and scale counts to mimic data from CCD
         pixel_grid *= 5000/pixel_grid.max()
         pixel_grid += 600
-        
+
         # Store output results
         self.x_loc = x_loc
         self.y_loc = y_loc
         self.actual_lattice = actual_lattice
         self.pixel_grid = pixel_grid
 
-        
-
     # useful for checking that each count sits in the middle of a pixel
     # isolates a particular part of the generated photon counts to more
     # easily see each pixel
-    def grid_plot(self, num_sites=1, invert_y = False):
+    def grid_plot(self, num_sites=1, invert_y=False):
         '''Plot the image (collected photons) on the camera.'''
         fig = plt.figure(figsize=(8, 8), dpi=100)
-        ax = fig.add_subplot(1,1,1)
-        xlims = (self.lattice_origin[0],self.lattice_origin[0]+self.N*self.M)
-        ylims = (self.lattice_origin[1],self.lattice_origin[1]+self.N*self.M)
-        coords = np.zeros((2,len(self.x_loc)),dtype=int)
-        coords[0,:] = self.x_loc
-        coords[1,:] = self.y_loc
+        ax = fig.add_subplot(1, 1, 1)
+        xlims = (self.lattice_origin[0], self.lattice_origin[0]+self.N*self.M)
+        ylims = (self.lattice_origin[1], self.lattice_origin[1]+self.N*self.M)
+        coords = np.zeros((2, len(self.x_loc)), dtype=int)
+        coords[0, :] = self.x_loc
+        coords[1, :] = self.y_loc
         coords = coords.T
-        lattice_coords = coords[(coords[:,0]>xlims[0])*(coords[:,0]<xlims[1])*(coords[:,1]>ylims[0])*(coords[:,1]<ylims[1]),:]
-        reduced_lattice_coords = lattice_coords[(lattice_coords[:,0] < (num_sites*self.M + xlims[0]))*(lattice_coords[:,1] < (num_sites*self.M + ylims[0]))]
+
+
+        lattice_coords = coords[(coords[:, 0] > xlims[0])*(coords[:, 0] < xlims[1])*(coords[:, 1] > ylims[0])*(coords[:, 1] < ylims[1]), :]  # noqa: E501
+        reduced_lattice_coords = lattice_coords[(lattice_coords[:, 0] < (num_sites * self.M + xlims[0]))*(lattice_coords[:, 1] < (num_sites*self.M + ylims[0]))]  # noqa: E501
 <<<<<<< Updated upstream
         im = plt.plot(reduced_lattice_coords.T[0], reduced_lattice_coords.T[1], 'ko', markersize=1,alpha=0.25) #plot counts
-        
+
         ax.set_xticks(np.arange(-0.5-1*self.M, (num_sites+1)*self.M+0.5, 1) + xlims[0]) #vertical lines as visual aid
         ax.set_yticks(np.arange(-0.5-1*self.M, (num_sites+1)*self.M+0.5, 1) + ylims[0]) #horizontal lines as visual aid
 =======
