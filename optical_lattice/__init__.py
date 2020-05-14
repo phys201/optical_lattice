14476b1ec1c7227696ae27632bee06c5819856ae
eknall <51799857+eknall@users.noreply.github.com>
2020-05-14 02:12:05 -0400

    Added spill_over model to analysis_models. Closes issue #40. Unfortunately we have not found a regime where the allowance of spill over outperforms the tiling approach. Also, as a result of having to generate (N*M)^2 points instead of just M^2 for the tiling case, it is much slower and scales poorly with N.

--- optical_lattice/__init__.py
+++ optical_lattice/__init__.py
@@ -2,3 +2,4 @@ from .lattice_generation import GeneratedLatticeImage
 from .lattice_analyzer import LatticeImageAnalyzer
 from .deconvolution_analysis import ConvolutionLatticeImageAnalyzer
 from .analysis_models import mixture_model
+from .analysis_models import mixture_model_spill_over
